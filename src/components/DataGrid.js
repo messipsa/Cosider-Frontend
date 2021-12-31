fb38f9893017cd6958e91ffd5ec108df37feebb7
Messipsa <im_tamazouzt@esi.dz>
2021-12-31 13:59:48 +0100

    refresh data with link fix

--- src/components/DataGrid.js
+++ src/components/DataGrid.js
@@ -1,175 +1,166 @@
 import React, { Component } from "react";
-import { Table , Loader , ButtonToolbar , Button ,ButtonGroup } from "rsuite";
-import { BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useRouteMatch,
-    useHistory
-} from 'react-router-dom';
+import { Table, Loader, ButtonToolbar, Button, ButtonGroup } from "rsuite";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useHistory,
+} from "react-router-dom";
 import axios from "axios";
 import Details from "./Details";
 import AddEmployee from "./AddEmployee";
-const {Cell ,HeaderCell , Column} = Table;
+const { Cell, HeaderCell, Column } = Table;
 
 const DataGrid = () => {
+  let { path, url } = useRouteMatch();
+  const history = useHistory();
+  const { innerHeight } = window;
+  const [employees, setEmployees] = React.useState([]);
+  const [loadingEmployees, setLoadingEmployees] = React.useState(false);
+  const [loadingDeletion, setLoadingDeletion] = React.useState(false);
+  //window.addEventListener('focus', () => getEmployees());
+  //window.location.reload();
 
-    let {path , url} = useRouteMatch();
-    const history = useHistory();
-    const {innerHeight} = window;
-    const [employees , setEmployees] = React.useState([]);
-    const [loadingEmployees , setLoadingEmployees] = React.useState(false);
-    const [loadingDeletion , setLoadingDeletion] = React.useState(false);
-    //window.addEventListener('focus', () => getEmployees());
-    //window.location.reload();
+  React.useEffect(() => {
+    getEmployees();
+  }, []);
 
-    React.useEffect(()=>{
-       getEmployees();
-    } , [])
+  const getEmployees = () => {
+    console.log("obligé");
+    setLoadingEmployees(true);
+    axios
+      .get("https://cosider-backend.herokuapp.com/api/employes")
+      .then((response) => {
+        setEmployees(response.data);
+        setLoadingEmployees(false);
+      })
+      .catch((err) => {
+        setLoadingEmployees(false);
+        console.log(err);
+      });
+  };
 
-    const getEmployees = () =>{
-        console.log("obligé")
-        setLoadingEmployees(true);
-        axios.get("https://cosider-backend.herokuapp.com/api/employes")
-        .then(response=>{
-             setEmployees(response.data);
-             setLoadingEmployees(false);
-            })
-            .catch(err=>{
-                setLoadingEmployees(false);
-                console.log(err);
-            })
-        
-    }
+  const addEmployee = () => {
+    history.push(`${path}/ajouter`);
+  };
 
+  return (
+    <>
+      <Switch>
+        <Route path={`${path}/details/:id`} component={Details} />
 
-    const addEmployee=()=>{
-        history.push(`${path}/ajouter`);
-    }
+        <Route path={`${path}/ajouter`} component={AddEmployee} />
 
-    
+        <Route path={path}>
+          <Button
+            onClick={addEmployee}
+            appearance="primary"
+            color="red"
+            style={{ margin: 30, marginLeft: window.innerWidth - 300 }}
+          >
+            Ajouter nouvel employé
+          </Button>
 
-    return (
-        <>
-          
-          <Switch>
+          <Table
+            virtualized
+            height={innerHeight - 200}
+            data={employees}
+            loading={loadingEmployees}
+            rowHeight={50}
+            style={{ marginLeft: 10 }}
+            onRowClick={(data) => {
+              console.log(data);
+            }}
+          >
+            <Column>
+              <HeaderCell>Matricule</HeaderCell>
+              <Cell dataKey="matricule"></Cell>
+            </Column>
 
-              
-              <Route path={`${path}/details/:id`} component={Details} />
+            <Column width={200}>
+              <HeaderCell>Nom</HeaderCell>
+              <Cell dataKey="nom"></Cell>
+            </Column>
 
-              <Route path={`${path}/ajouter`} component={AddEmployee}/>
-                  
-              <Route path={path} >
+            <Column>
+              <HeaderCell>Projet</HeaderCell>
+              <Cell dataKey="projet.entite"></Cell>
+            </Column>
 
-              <Button
-            onClick = {addEmployee}
-              appearance="primary"
-              color="red"
-              style={{margin : 30 , marginLeft : window.innerWidth - 300}}>
-               Ajouter nouvel employé
-              </Button>
+            <Column width={290} fixed="right">
+              <HeaderCell></HeaderCell>
+              <Cell>
+                {(rowData) => {
+                  const showDetails = () => {
+                    history.push((path = `${path}/details/${rowData._id}`), {
+                      user: rowData,
+                    });
+                    console.log(rowData);
+                    localStorage.setItem("employeeId", rowData._id);
+                    localStorage.setItem(
+                      "employeeData",
+                      JSON.stringify(rowData)
+                    );
+                    console.log(localStorage.getItem("employeeData"));
+                  };
 
-                  <Table
-                  virtualized
-                  height={innerHeight - 200}
-                 
-                  data={employees}
-                  loading={loadingEmployees}
-                  rowHeight={50}
-                  style={{marginLeft : 10}}
-                  onRowClick={(data)=>{
-                      console.log(data)
-                  }}>
-                      <Column >
-                      <HeaderCell>Matricule</HeaderCell>
-                      <Cell dataKey="matricule"></Cell>
-                      </Column>
+                  const deleteEmployee = () => {
+                    setLoadingDeletion(true);
+                    console.log("identifiant : " + rowData._id);
+                    var data = "";
 
-                      <Column width={200}>
-                      <HeaderCell>Nom</HeaderCell>
-                      <Cell dataKey="nom"></Cell>
-                      </Column>
+                    var config = {
+                      method: "delete",
+                      url: `https://cosider-backend.herokuapp.com/api/employes/supprimer/${rowData._id}`,
+                      headers: {},
+                      data: data,
+                    };
 
-                      <Column >
-                      <HeaderCell>Projet</HeaderCell>
-                      <Cell dataKey="projet.entite"></Cell>
-                      </Column>
+                    axios(config)
+                      .then((response) => {
+                        console.log(JSON.stringify(response.data));
+                        alert("c'est une plair ");
+                        setLoadingDeletion(false);
+                      })
+                      .catch((error) => {
+                        console.log(error);
+                        alert("ce n'est pas une plaisir");
+                        setLoadingDeletion(false);
+                      });
+                    // window.location.reload()
+                  };
+                  return (
+                    <>
+                      <ButtonToolbar style={{ marginTop: -5 }} align="right">
+                        <Button
+                          onClick={showDetails}
+                          appearance="primary"
+                          color="red"
+                        >
+                          Details
+                        </Button>
 
-                      <Column width={290} fixed="right">
-                      <HeaderCell></HeaderCell>
-                      <Cell>
-                         { (rowData) =>{
-                             const showDetails = () =>{
-                                 history.push(path=`${path}/details/${rowData._id}` , {user : rowData})
-                             }
+                        <Button
+                          style={{ marginLeft: 15 }}
+                          onClick={deleteEmployee}
+                          appearance="primary"
+                          color="red"
+                        >
+                          Supprimer
+                        </Button>
+                      </ButtonToolbar>
+                    </>
+                  );
+                }}
+              </Cell>
+            </Column>
+          </Table>
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
-                             const deleteEmployee = ()=> {
-                                setLoadingDeletion(true);
-                                console.log("identifiant : "+ rowData._id)
-                                var data = '';
-                        
-                        var config = {
-                          method: 'delete',
-                          url: `https://cosider-backend.herokuapp.com/api/employes/supprimer/${rowData._id}`,
-                          headers: { },
-                          data : data
-                        };
-                        
-                        axios(config)
-                        .then(response=> {
-                            
-                          console.log(JSON.stringify(response.data));
-                          alert("c'est une plair ")
-                          setLoadingDeletion(false)
-                        })
-                        .catch(error=> {
-                          console.log(error);
-                          alert("ce n'est pas une plaisir")
-                          setLoadingDeletion(false)
-                        });
-                       // window.location.reload()
-                         }
-                             return(
-                                 <>
-                                <ButtonToolbar style={{ marginTop: -5 } } align='right'>
-                                
-    
-  
-                                <Button
-                                  onClick={showDetails}
-                                  appearance="primary"
-                                  color="red"
-                                  
-                                >
-                                  Details
-                                </Button>
-                              
-
-                              
-                                <Button
-                                  style={{ marginLeft: 15 } }
-                                  onClick={deleteEmployee}
-                                  appearance="primary"
-                                  color="red"
-                                  
-                                >
-                                  Supprimer
-                                </Button>
-                                
-                              </ButtonToolbar>
-                              </>
-                             );
-                         }
-                         } 
-                      </Cell>
-                      </Column>
-        
-
-                  </Table>
-                  </Route>
-          </Switch>
-          </>
-    )
-}
-
-export default DataGrid
+export default DataGrid;
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,53 +1,46 @@
-import React  from "react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import axios from "axios";
-import {
-    Panel,
-    Input,
-    FlexboxGrid,
-    Button,
-    Message,
-    Loader
-} from "rsuite";
+import { Panel, Input, FlexboxGrid, Button, Message, Loader } from "rsuite";
 import FlexboxGridItem from "rsuite/esm/FlexboxGrid/FlexboxGridItem";
 import { useLocation, useRouteMatch } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import Contrat from './Contrat'
+import Contrat from "./Contrat";
 
 const styles = {
-    marginTop:5,
-    marginBottom:5,
-}
-
+  marginTop: 5,
+  marginBottom: 5,
+};
 
 const Details = () => {
-    const {path , url} = useRouteMatch()
+  const { path, url } = useRouteMatch();
   const history = useHistory();
-    const location = useLocation();
-    const identifiant = JSON.stringify(useParams().id);
-  const employeeId = localStorage.getItem('employeeId');
-  const employeeData = JSON.parse(localStorage.getItem('employeeData'));
-    //const [employee ,setEmployee] = React.useState([]);
-  const [employee, setEmployee] = React.useState((employeeId && history.location.state.user && employeeId === history?.location?.state?.user?._id) ? employeeData : history.location.state.user); /*React.useMemo(()=>{
-      return location.state.user
-    },[identifiant])*/
-    const date_naissance = String(location.state.user.date_naissance);
-    const res = date_naissance.substring(0,10);
-    const [isReadOnly , setIsReadOnly] = React.useState(true);
-  const [nom, setNom] = React.useState(employee?.nom);
-  const [adress, setAdress] = React.useState(employee?.adresse)
-  const [lieuNaiss, setLieuNaiss] = React.useState(employee?.lieu_naissance);
-  const [dateNaiss, setDateNaiss] = React.useState(employee?.date_naissance?.substring(0, 10));
-    const [loadingModify , setLoadingModify] = React.useState(false);
-    const [Travailleur , setTravailleur] = React.useState(false);
+  const location = useLocation();
+  console.log(localStorage.getItem("employeeId"));
+  const identifiant = JSON.stringify(useParams().id);
+  const employeeId = localStorage.getItem("employeeId");
+  const employeeData = localStorage.getItem("employeeData");
+  console.log(JSON.parse(employeeData));
+  //const [employee ,setEmployee] = React.useState([]);
+  const [employee, setEmployee] = React.useState(employeeData);
 
-   
+  const [isReadOnly, setIsReadOnly] = React.useState(true);
+  const [nom, setNom] = React.useState(JSON.parse(employee)?.nom);
+  const [adress, setAdress] = React.useState(JSON.parse(employee)?.adresse);
+  const [lieuNaiss, setLieuNaiss] = React.useState(
+    JSON.parse(employee)?.lieu_naissance
+  );
+  const [dateNaiss, setDateNaiss] = React.useState(
+    JSON.parse(employee)?.date_naissance?.substring(0, 10)
+  );
+  const [loadingModify, setLoadingModify] = React.useState(false);
+  const [Travailleur, setTravailleur] = React.useState(false);
 
-    React.useEffect(()=>{
-      //history.goBack();
-      //history.goForward();
-    },[])
-    /*React.useEffect(()=>{
+  React.useEffect(() => {
+    //history.goBack();
+    //history.goForward();
+  }, []);
+  /*React.useEffect(()=>{
         getEmployee();
     } , [])
 
@@ -66,89 +59,88 @@ const Details = () => {
         })
     }   */
 
+  const modifyEmployee = () => {
+    setIsReadOnly(false);
+  };
 
+  const saveModification = () => {
+    setIsReadOnly(true);
+    var data = JSON.stringify({
+      nom: nom,
+      adresse: adress,
+      date_naissance: dateNaiss,
+      lieu_naissance: lieuNaiss,
+    });
 
-    const modifyEmployee =()=>{
-        setIsReadOnly(false);
-    }
+    var config = {
+      method: "put",
+      url: `https://cosider-backend.herokuapp.com/api/employes/modifier/${
+        JSON.parse(employee)._id
+      }`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
 
+    setLoadingModify(true);
+    axios(config)
+      .then(function (response) {
+        localStorage.setItem("employeeId", response.data._id);
+        localStorage.setItem("employeeData", JSON.stringify(response.data));
+        setLoadingModify(false);
+        alert("c est une plaisir");
+      })
+      .catch(function (error) {
+        setTimeout(setLoadingModify(false), 3000);
+        alert("ce n'est pas une plaisir");
+        return (
+          <Message showIcon type="error">
+            Error
+          </Message>
+        );
+      });
+  };
 
-    const saveModification = ()=>{
-       
-        setIsReadOnly(true);
-        var data = JSON.stringify({
-        "nom": nom,
-        "adresse": adress,
-        "date_naissance": dateNaiss,
-        "lieu_naissance": lieuNaiss})
-          
-          var config = {
-            method: 'put',
-            url: `https://cosider-backend.herokuapp.com/api/employes/modifier/${employee._id}`,
-            headers: { 
-              'Content-Type': 'application/json'
-            },
-            data : data
-          };
-          
-          setLoadingModify(true);
-          axios(config)
-          .then(function (response) {
-            localStorage.setItem('employeeId', response.data._id);
-            localStorage.setItem('employeeData', JSON.stringify(response.data));
-            setLoadingModify(false)
-            alert("c est une plaisir");
-        })
-            .catch(function (error) {
-            setTimeout(setLoadingModify(false), 3000);
-            alert("ce n'est pas une plaisir")
-            return(
-                <Message showIcon type="error">
-                  Error
-                </Message>
-            )
-          });
+  return (
+    <>
+      <Panel header="Information employé" bordered style={{ marginTop: 20 }}>
+        <FlexboxGrid justify="space-between">
+          <FlexboxGridItem colspan={7}>
+            <label>Matricule:</label>
+            <Input
+              readOnly
+              style={styles}
+              value={JSON.parse(employee).matricule}
+            />
+          </FlexboxGridItem>
 
-          
-    }
+          <FlexboxGridItem colspan={7}>
+            <label>Nom:</label>
+            <Input
+              readOnly={isReadOnly}
+              value={nom}
+              style={styles}
+              onChange={(newValue) => {
+                setNom(newValue);
+              }}
+            />
+          </FlexboxGridItem>
 
-    
+          <FlexboxGridItem colspan={7}>
+            <label>Date de naiss:</label>
+            <Input
+              readOnly={isReadOnly}
+              value={dateNaiss}
+              style={styles}
+              onChange={(newValue) => {
+                setDateNaiss(newValue);
+              }}
+            />
+          </FlexboxGridItem>
+        </FlexboxGrid>
 
-    return (
-       <>
-       <Panel header="Information employé" bordered style={{marginTop : 20}}>
-         <FlexboxGrid justify="space-between">
-         
-         <FlexboxGridItem colspan={7}>
-         <label>Matricule:</label>
-         <Input readOnly style={styles} value={employee.matricule}/>
-         </FlexboxGridItem>
-
-         <FlexboxGridItem colspan={7}>
-             <label>Nom:</label>
-             <Input 
-             readOnly={isReadOnly} 
-             value={nom}
-             style={styles}
-             onChange={(newValue)=>{
-               setNom(newValue);
-             }}/>
-         </FlexboxGridItem>
-
-         <FlexboxGridItem colspan={7}>
-             <label>Date de naiss:</label>
-             <Input 
-             readOnly={isReadOnly} 
-             value={dateNaiss}
-             style={styles}
-             onChange={(newValue)=>{
-               setDateNaiss(newValue);
-             }}/>
-         </FlexboxGridItem>
-         </FlexboxGrid>
-
-     
-         <label>Lieu de naiss:</label>
+        <label>Lieu de naiss:</label>
         <Input
           readOnly={isReadOnly}
           style={styles}
@@ -163,44 +155,45 @@ const Details = () => {
           readOnly={isReadOnly}
           style={styles}
           value={adress}
-          onChange={(newValue) => { 
+          onChange={(newValue) => {
             setAdress(newValue);
           }}
         />
 
-    <FlexboxGrid justify="end">
-      <FlexboxGrid.Item >
-         <Button 
-         style={{marginRight : 10 , marginTop:20 , width:100}}
-         appearance="primary"
-         onClick={isReadOnly ? modifyEmployee : saveModification}>
-            { isReadOnly ?(
-             <p>Modifier</p>
-            ) : loadingModify ? (
+        <FlexboxGrid justify="end">
+          <FlexboxGrid.Item>
+            <Button
+              style={{ marginRight: 10, marginTop: 20, width: 100 }}
+              appearance="primary"
+              onClick={isReadOnly ? modifyEmployee : saveModification}
+            >
+              {isReadOnly ? (
+                <p>Modifier</p>
+              ) : loadingModify ? (
                 <Loader />
-              ): (
+              ) : (
                 <p>Enregistrer</p>
-            )
-            }
-         </Button>
-      </FlexboxGrid.Item>
+              )}
+            </Button>
+          </FlexboxGrid.Item>
 
-      <FlexboxGridItem>
-          <Button
-          style={{marginTop : 20 , marginRight :10 , width:100}}
-          appearance="ghost" onClick={()=>{
-              history.goBack()
-          }}>
+          <FlexboxGridItem>
+            <Button
+              style={{ marginTop: 20, marginRight: 10, width: 100 }}
+              appearance="ghost"
+              onClick={() => {
+                history.goBack();
+              }}
+            >
               Supprimer
-           </Button>
-      </FlexboxGridItem>
-    </FlexboxGrid>
-         
-       </Panel>
+            </Button>
+          </FlexboxGridItem>
+        </FlexboxGrid>
+      </Panel>
 
-       <Contrat employee = {employee} history={history} />
-       </>
-    )
-}
+      <Contrat employee={employee} history={history} />
+    </>
+  );
+};
 
-export default Details
+export default Details;
