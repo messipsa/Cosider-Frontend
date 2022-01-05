e37fcfbdb193b36ab67d791e589b660d319ddbaf
Messipsa <im_tamazouzt@esi.dz>
2022-01-05 11:40:35 +0100

    register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,49 @@
 import React from "react";
-import { Panel, FlexboxGrid, Input, InputGroup, Button } from "rsuite";
-import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { Panel, FlexboxGrid, Input, InputGroup, Button, Loader } from "rsuite";
+import { AiFillEye, AiFillEyeInvisible, AiOutlineMail } from "react-icons/ai";
 import { Link, useHistory, useRouteMatch } from "react-router-dom";
+import axios from "axios";
+
 const Login = () => {
   const history = useHistory();
   const { path, url } = useRouteMatch();
   const [visible, setVisible] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [loadingConnex, setLoadingConnex] = React.useState(false);
 
   const handleChange = () => {
     setVisible(!visible);
   };
 
   const login = () => {
-    history.push("/app");
+    setLoadingConnex(true);
+    var data = JSON.stringify({
+      email: email,
+      password: password,
+    });
+
+    var config = {
+      method: "post",
+      url: "http://localhost:5000/api/user/login",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        setLoadingConnex(false);
+        alert("Connexion réussie");
+        history.push("/home");
+      })
+      .catch(function (error) {
+        setLoadingConnex(false);
+        alert("Echec de la connexion");
+        console.log(error);
+      });
   };
 
   return (
@@ -24,19 +55,36 @@ const Login = () => {
       <FlexboxGrid>
         <label style={{ margin: 10 }}>Email</label>
         <InputGroup>
-          <InputGroup.Addon> @</InputGroup.Addon>
-          <Input />
+          <InputGroup.Addon>
+            {" "}
+            <AiOutlineMail />
+          </InputGroup.Addon>
+          <Input
+            onChange={(newValue) => {
+              setEmail(newValue);
+            }}
+          />
         </InputGroup>
         <label style={{ margin: 10 }}>Mot de passe</label>
         <InputGroup inside>
-          <Input type={visible ? "text" : "password"} />
+          <Input
+            type={visible ? "text" : "password"}
+            onChange={(newValue) => {
+              setPassword(newValue);
+            }}
+          />
           <InputGroup.Button onClick={handleChange}>
             {visible ? <AiFillEye /> : <AiFillEyeInvisible />}
           </InputGroup.Button>
         </InputGroup>
 
-        <Button style={{ margin: 15, marginLeft: 240 }} onClick={login}>
-          Se Connecter
+        <Button
+          style={{ margin: 15, marginLeft: 240, width: 130 }}
+          onClick={login}
+          color="red"
+          appearance="primary"
+        >
+          {!loadingConnex ? <p>Se Connecter</p> : <Loader />}
         </Button>
         <p style={{ marginLeft: 120, justifyContent: "center" }}>
           Si vous n'avez pas de compte veuillez vous{" "}
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,111 @@
+import React from "react";
+import { Panel, FlexboxGrid, Input, InputGroup, Button, Loader } from "rsuite";
+import {
+  AiFillEye,
+  AiFillEyeInvisible,
+  AiOutlineUser,
+  AiOutlineMail,
+} from "react-icons/ai";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 const Register = () => {
+  const history = useHistory();
+  const [visible, setVisible] = React.useState(false);
+  const [pseudo, setPseudo] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [loadingRegister, setLoadingRegister] = React.useState(false);
+
+  const handleChange = () => {
+    setVisible(!visible);
+  };
+
+  const register = () => {
+    setLoadingRegister(true);
+    var data = JSON.stringify({
+      userName: pseudo,
+      email: email,
+      password: password,
+    });
+
+    var config = {
+      method: "post",
+      url: "http://localhost:5000/api/user/register",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        setLoadingRegister(false);
+        alert("Utilisateur créé avec succès");
+        history.push("/");
+      })
+      .catch(function (error) {
+        setLoadingRegister(false);
+        alert("Echec de la création de compte");
+        console.log(error);
+      });
+  };
+
   return (
-    <div>
-      <h1>Salim Tir Lil</h1>
-    </div>
+    <Panel
+      header="S'inscrire"
+      bordered
+      style={{ marginRight: 450, marginTop: 120, marginLeft: 450 }}
+    >
+      <FlexboxGrid>
+        <label style={{ margin: 10 }}>Nom d'utilisateur</label>
+        <InputGroup>
+          <InputGroup.Addon>
+            {" "}
+            <AiOutlineUser />
+          </InputGroup.Addon>
+          <Input
+            onChange={(newValue) => {
+              setPseudo(newValue);
+            }}
+          />
+        </InputGroup>
+
+        <label style={{ margin: 10 }}>Email</label>
+        <InputGroup>
+          <InputGroup.Addon>
+            {" "}
+            <AiOutlineMail />
+          </InputGroup.Addon>
+          <Input
+            onChange={(newValue) => {
+              setEmail(newValue);
+            }}
+          />
+        </InputGroup>
+        <label style={{ margin: 10 }}>Mot de passe</label>
+        <InputGroup inside>
+          <Input
+            type={visible ? "text" : "password"}
+            onChange={(newValue) => {
+              setPassword(newValue);
+            }}
+          />
+          <InputGroup.Button onClick={handleChange}>
+            {visible ? <AiFillEye /> : <AiFillEyeInvisible />}
+          </InputGroup.Button>
+        </InputGroup>
+
+        <Button
+          style={{ margin: 15, marginLeft: 240, width: 100 }}
+          onClick={register}
+          color="red"
+          appearance="primary"
+        >
+          {!loadingRegister ? <p>S'inscrire</p> : <Loader />}
+        </Button>
+      </FlexboxGrid>
+    </Panel>
   );
 };
 
