916e31d8661f8049fe85d447d8211d475cb2f43c
Messipsa <im_tamazouzt@esi.dz>
2022-01-10 22:10:08 +0100

    adding classification to contract

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -9,7 +10,10 @@ import {
 import { Button } from "rsuite";
 import Login from "./components/Login";
 import App from "./App";
+import NavCust from "./components/NavCust";
 import Register from "./components/Register";
+import DataGrid from "./components/DataGrid";
+import Projets from "./components/Projets";
 const Main = () => {
   return (
     <Router>
@@ -17,6 +21,8 @@ const Main = () => {
         <Route exact path="/" component={Login} />
         <Route path="/home" component={App} />
         <Route path="/register" component={Register} />
+        <Route path="/employes" component={DataGrid} />
+        <Route path="/projets" component={Projets} />
       </Switch>
     </Router>
   );
